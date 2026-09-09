0dd5d2124d42f812dc4f206faf95e20850a8790d
piyushsingh <piyush.singh6730@gmail.com>
2026-09-09 16:20:13 +0530

    Update CORS for deployment

--- server/app.js
+++ server/app.js
@@ -8,12 +8,10 @@ const postRoutes = require("./routes/posts");
 const app = express();
 
 // Middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    })
-);
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
