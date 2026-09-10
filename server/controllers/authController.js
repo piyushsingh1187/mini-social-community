234b3d7799980d236503f7da9c8f770a0d8a5eee
piyushsingh <piyush.singh6730@gmail.com>
2026-09-10 19:57:00 +0530

    Update JWT cookie for production

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -93,10 +93,18 @@ const login = async (req, res) => {
         );
 
         // 5. Store JWT in HTTP-only cookie
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: false,
+        //     sameSite: "lax",
+        //     maxAge: 24 * 60 * 60 * 1000
+        // });
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+            sameSite: process.env.NODE_ENV === "production"
+                ? "none"
+                : "lax"
         });
 
         // 6. Send response
